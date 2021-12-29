a36bcf41f457cc5071f2ed40108b82759963b41c
phiarchitect <phi@phiarchitect.com>
2021-12-28 18:11:30 -0800

    nit project files

---
+++ phi_midi/__init__.py
@@ -0,0 +1,14 @@
+from mido import Message, MidiFile, MidiTrack, MetaMessage
+
+
+chords = {
+  'Major': [0, 4, 7],
+  'Major7': [0, 4, 7, 11],
+  'Dominant7': [0, 4, 7, 10],
+  'Minor': [0, 3, 7],
+  'Minor7': [0, 3, 7, 10],
+  'Diminished': [0, 3, 6],
+  'Sus2': [0, 2, 7],
+  'Sus4': [0, 5, 7],
+}
+
