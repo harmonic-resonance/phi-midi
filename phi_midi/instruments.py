340384060c8e93c4ea41826f423f8dafea00b9da
phiarchitect <phi@phiarchitect.com>
2022-01-19 08:08:49 -0800

    add instruments

---
+++ phi_midi/instruments.py
@@ -0,0 +1,13 @@
+"""
+wrapper for managing instruments
+"""
+
+class Instrument:
+
+    """Docstring for Instrument. """
+
+    def __init__(self, name, channel):
+        """TODO: to be defined. """
+        .__init__(self)
+
+        
