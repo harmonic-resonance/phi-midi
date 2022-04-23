51fe6d7236bd397e0bd56e7a202c31625640336a
phiarchitect <phi@phiarchitect.com>
2022-04-23 06:01:07 -0700

    add drum kits

--- phimidi/percussions/__init__.py
+++ phimidi/percussions/__init__.py
@@ -17,14 +17,38 @@ def make_low_tom(mf):
 def make_high_tom(mf):
     return Percussion(mf, P.high_tom)
 
+def make_floor_tom(mf):
+    return Percussion(mf, P.low_floor_tom)
+
 def make_snare(mf):
     return Percussion(mf, P.acoustic_snare)
 
 def make_hihat_closed(mf):
     return Percussion(mf, P.closed_hi_hat)
 
+def make_hihat_open(mf):
+    return Percussion(mf, P.open_hi_hat)
+
+def make_crash(mf):
+    return Percussion(mf, P.crash_cymbal_1)
+
 def make_ride(mf):
     return Percussion(mf, P.ride_cymbal_1)
 
+def make_kit_1(mf):
+    tick = make_tick(mf)
+    kick = make_kick(mf)
+    snare = make_snare(mf)
+    hihat_c = make_hihat_closed(mf)
+    hihat_o = make_hihat_open(mf)
+    ride = make_ride(mf)
 
+    return tick, kick, snare, hihat_c, hihat_o, ride
+    
+def make_kit_toms(mf):
+    kit_1 = make_kit_1(mf)
+    low_tom = make_low_tom(mf)
+    high_tom = make_high_tom(mf)
+    floor_tom = make_floor_tom(mf)
 
+    return *kit_1, low_tom, high_tom, floor_tom
