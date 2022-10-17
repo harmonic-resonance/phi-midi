4b1ffdb0eb419153f738b776e1eb5e55dfcbf9ab
phiarchitect <phi@phiarchitect.com>
2022-10-17 04:37:41 -0700

    update track

--- thelio_button.py
+++ thelio_button.py
@@ -3,8 +3,6 @@ script for generating accompaniment for Thelio videos
 :bpm: based on the observed timing of the suspend light
 """
 import phimidi as pm
-import itertools as itertools
-import random as random
 from rich import print as log
 
 PROJECT = 'thelio'
@@ -15,7 +13,24 @@ key = 'D'
 
 chords = pm.progressions.thelio(root)
 
-for verse in ["tower", "corner"]:
+part = pm.Part(PROJECT, "button_dark", bpm=bpm, root=root, key=key)
+M = part.measure_ticks()
+
+choir_ooh = part.add_choir_ooh()
+choir_aah = part.add_choir_aah()
+
+choir_ooh.set_notes(chords[0][1], M/2)
+choir_aah.set_rest(M/2)
+choir_aah.set_notes(chords[1][1], M)
+choir_ooh.set_rest(M/2)
+
+part.save()
+part.play()
+part.convert()
+
+#  exit()
+
+for verse in ["button_blink"]:
     part = pm.Part(PROJECT, verse, bpm=bpm, root=root, key=key)
     M = bpM * part.ticks_per_beat  # ticks per Measure
 
@@ -29,7 +44,8 @@ for verse in ["tower", "corner"]:
     #  tick = pm.make_tick(part)
     tick = pm.make_low_tom(part)
 
-    choir = part.add_choir_swell()
+    choir = part.add_choir_ooh()
+
 
     for chord_num, (chord_name, chord) in enumerate(chords):
         if chord_num in [0, 3]:
@@ -38,7 +54,7 @@ for verse in ["tower", "corner"]:
             rhythm = pm.patterns.latin.rhumba
         if chord_num in [2, 5]:
             rhythm = pm.patterns.latin.son_clave
-        measures = 8
+        measures = 4
         for m in range(measures):
             if m == 0:
                 # first
@@ -71,7 +87,7 @@ for verse in ["tower", "corner"]:
         vibes.set_rest(2 * M)
         vibes.set_notes(chord3, M, offset=offset, velocity=55)
         vibes.set_notes(chord4, M/2, offset=offset, velocity=65)
-        vibes.set_notes(chord4, 4*M + M/2, offset=offset, velocity=65)
+        vibes.set_notes(chord4, M/2, offset=offset, velocity=65)
 
         # strings
         if verse == 'corner':
